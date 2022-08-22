4d9b6390a4a109bbc2bb8dfb4b9ce8053cb798e9
katy.korotkih <katy_korotkih@mail.ru>
2022-08-22 13:25:11 +0300

    Correction added

--- HW_009/Program.cs
+++ HW_009/Program.cs
@@ -24,7 +24,6 @@ Console.Write("Input second number: ");
 int m = Convert.ToInt32(Console.ReadLine());
 ShowNums(n, m);
 
-
 //Задача 66: Задайте значения M и N. 
 //Напишите программу, которая найдёт сумму 
 //натуральных элементов в промежутке от M до N.
@@ -61,7 +60,7 @@ int Akkerman(int m, int n)
     if (m == 0 ) return n + 1;
     if (m > 0 && n == 0) return Akkerman(m - 1, 1);
     if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
-    return 1;
+    return 0;
 }
 
 Console.Write("Введите первое число: ");
@@ -70,5 +69,5 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Akkerman(m,n));
+Console.WriteLine($"Результат вычисления функции Аккермана: {Akkerman(m,n)}");
 */
